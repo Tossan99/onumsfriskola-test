c915ae27b9849bd4c0f0f94fa9f6bf13cfe20be5
Erik <erik-guldbrand@hotmail.com>
2024-06-16 14:44:27 +0200

    Add model to add school classes and school classes page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import SchoolClass
 
-def view_blog(request):
+
+def view_all_school_classes(request):
+    """ 
+    Renders all school classes and posts
     """
-    View to render blog page
-    """
+
+    school_classes = SchoolClass.objects.all()
+
     context = {
-
+        "school_classes": school_classes,
     }
 
-    return render(request, 'blog/blog.html', context)
+    return render(request, "blog/school_classes.html", context)
+
+def view_school_class(request, slug):
+    """ 
+    Renders a school class and it's related posts
+    """
+
+    school_classes = SchoolClass.objects.all()
+    school_class = get_object_or_404(school_classes, slug=slug)
+
+    context = {
+        "school_class": school_class,
+        "school_classes": school_classes,
+    }
+
+    return render(request, "blog/school_classes.html", context)
