ea2a9121617a79a6d6d7b0b8266ae4110f53bed6
Erik <erik-guldbrand@hotmail.com>
2024-06-16 12:41:13 +0200

    Add content boxes css for document page

--- onumsfriskola/urls.py
+++ onumsfriskola/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('home.urls')),
     path('blogg', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
