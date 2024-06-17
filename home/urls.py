7919048b0d57cb577d5793989efb212a57da0f1c
KlaraM <klara.ma@outlook.com>
2024-06-17 21:08:21 +0200

    Add school classes on classes page.

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_home, name='home'),
-    path('dokument', views.view_documents, name='documents'),
-    path('skolan', views.view_about, name='about'),
-    path('personal', views.view_staff, name='staff'),
+    path('dokument/', views.view_documents, name='documents'),
+    path('skolan/', views.view_about, name='about'),
+    path('personal/', views.view_staff, name='staff'),
 ]
